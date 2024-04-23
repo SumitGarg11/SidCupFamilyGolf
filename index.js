c5ac8d880ae25e459a0addde8887785d01a550e2
Sumit Garg <118594912+SumitGarg11@users.noreply.github.com>
2024-04-23 14:02:04 +0530

    index.js

--- index.js
+++ index.js
@@ -62,20 +62,7 @@ gsap.from("#about-us img,#about-us-in", {
     scrub: 1,
   },
 });
-gsap.from(".card", {
-  scale: 0.8,
-  // opacity:0,
-  duration: 1,
-  stagger: 0.1,
-  scrollTrigger: {
-    trigger: ".card",
-    scroller: "body",
-    // markers:false,
-    start: "top 70%",
-    end: "top 65%",
-    scrub: 1,
-  },
-});
+
 gsap.from("#colon1", {
   y: -70,
   x: -70,
